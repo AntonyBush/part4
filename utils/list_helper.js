d7d1edd72c685a10cb89608347d0842ef43bdf5d
Antony <antonybush95@gmail.com>
2023-08-29 00:01:06 +0530

    Ex 4.3 - 4.6

---
+++ utils/list_helper.js
@@ -0,0 +1,39 @@
+let _ = require('lodash');
+
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  if (blogs.length !== 0)
+    return blogs.reduce((total, curr) => total + curr.likes, 0);
+  else return 0;
+};
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length !== 0) {
+    const favBlog = blogs.reduce(
+      (prev, curr) => (prev.likes > curr.likes ? prev : curr),
+      0
+    );
+    return {
+      title: favBlog.title,
+      author: favBlog.author,
+      likes: favBlog.likes,
+    };
+  } else return {};
+};
+
+const mostBlogs = (blogs) => {
+  return _(blogs)
+    .groupBy('author')
+    .map((g, key) => {
+      return {
+        author: key,
+        blogs: g.length,
+      };
+    })
+    .maxBy('blogs');
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs };
